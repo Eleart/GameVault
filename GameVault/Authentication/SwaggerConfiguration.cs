ce7771b42306ec00aa87845331bdf2605de42d99
Thomas.petit261 <Thomas.petit261@gmail.com>
2025-05-16 21:36:35 +0200

    Configure swagger with authentication

---
+++ GameVault/Authentication/SwaggerConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace GameVault.Authentication;
+
+public static class SwaggerConfiguration
+{
+    public static OpenApiSecurityScheme OpenApiSecurityScheme => new()
+    {
+        Description = "The Api Key to access the API",
+        Type = SecuritySchemeType.ApiKey,
+        Name = AuthConstants.ApiKeyHeaderName,
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    };
+
+    private static OpenApiSecurityScheme Scheme => new()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+
+    public static OpenApiSecurityRequirement Requirement => new()
+    {
+        {Scheme, new List<string>() }
+    };
+}
--- GameVault/Program.cs
+++ GameVault/Program.cs
@@ -1,4 +1,5 @@
 using GameVault.Authentication;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("ApiKey", SwaggerConfiguration.OpenApiSecurityScheme);
+
+    c.AddSecurityRequirement(SwaggerConfiguration.Requirement);
+});
+
 builder.Services.AddScoped<ApiKeyAuthFilter>();
 var app = builder.Build();
 
